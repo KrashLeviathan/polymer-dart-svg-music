af02d2ce14e0c2fcceeb141cbc00f422aeef12ff
KrashLeviathan <nathankarasch@gmail.com>
2016-06-23 06:23:02 -0500

    Moved polymer elements into their own folder

--- web/index.dart
+++ web/index.dart
@@ -1,7 +1,7 @@
 // Copyright (c) 2016, Nathan Karasch. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-import 'package:svg_polymer_playground/main_app.dart';
+import 'package:svg_polymer_playground/polymer-elements/main_app.dart';
 import 'package:polymer/polymer.dart';
 
 /// [MainApp] used!
